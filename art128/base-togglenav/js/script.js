2c556755209320ba15680d88a3420e271c175984
catriveros <riveros@hawaii.edu>
2023-03-22 00:50:17 -1000

    java script

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //JAVASCRIPT FUNCTIONS GO HERE
-    console.log('JS IS READY')
-
-});
+    //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
+    const menubutton  = document.querySelector('.menu-button');
+    const menunav  = document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function() {
+        if (menunav.getAttribute('data-navstate') === 'open') {
+           //if true do this
+            menunav.setAttribute('data-navstate', 'closed');
+        } else {
+            //elese (if flase) do this:
+            menunav.getAttribute('data-navstate', 'open');
+        };
+    });
+  
+  });
