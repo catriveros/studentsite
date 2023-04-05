f194fd859e32c4c1a66ee33c994d35e2248dcb1d
catriveros <riveros@hawaii.edu>
2023-04-04 23:49:31 -1000

    paragraph entered

--- art128/blue mind surf therapy/js/script.js
+++ art128/blue mind surf therapy/js/script.js
@@ -1,20 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
+//modified from (added offset to) orignal anchor-scroll by Benjamin De Cock (https://github.com/bendc)
+//https://github.com/bendc/anchor-scroll
+
+
+document.addEventListener('DOMContentLoaded', function () {
 
     //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
-    const menubutton  = document.querySelector('.menu-button');
-    const menunav  = document.querySelector('.toggle-nav');
-    menubutton.addEventListener('click', function() {
+    const menubutton = document.querySelector('.menu-button');
+    const menunav = document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function () {
         if (menunav.getAttribute('data-navstate') === 'open') {
-           // if true do this:
-           menunav.setAttribute('data-navstate', 'closed');
-
+            // if true do this
+            menunav.setAttribute('data-navstate', 'closed');
         } else {
             // else (if false) do this:
             menunav.setAttribute('data-navstate', 'open');
-
         };
-
-      
     });
-  
-  });
+
+    //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
+    var stickynavlinks = document.querySelectorAll(".sticky nav a");
+    for (var i = 0; i < stickynavlinks.length; i++) {
+        stickynavlinks[i].onclick = function () {
+            menunav.setAttribute('data-navstate', 'closed');
+        }
+    };
+
+});
